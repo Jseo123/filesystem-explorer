e8f1be0efde1eaf775a18b1bedf9aa5191cb63a6
gcachonc <g.cachon.c@gmail.com>
2021-12-17 17:32:19 +0100

    midle box

--- functions.php
+++ functions.php
@@ -2,38 +2,41 @@
 function callFolders()
 {
     $path = "root/";
-    if (is_dir($path)) {
-        if ($dh = opendir($path)) {
-            while (($file = readdir($dh)) !== false) {
-                if ($file !== "." && $file !== ".." &&  !pathinfo($file, PATHINFO_EXTENSION)) {
-                    $path2 = "root/$file";
-                    echo "<div><a href='index.php?files=$path2'>" . $file . "</a></div>";
-                if (is_dir($path2)) {
-                    if ($dh2 = opendir($path2)) {
-                        while (($file2 = readdir($dh2)) !== false) {
-                            if ($file2 !== "." && $file2 !== ".." &&  !pathinfo($file2, PATHINFO_EXTENSION)) {
-                                $path3 = "root/$file/$file2";
-                                echo "<div  class='subdirectories'><a href='index.php?files=$path3' class='anchor'>" . $file2 . "</a></div>";
-                            }
-                        }
+    if (is_dir($path) && $dh = opendir($path)) {
+        while (($file = readdir($dh)) !== false) {
+            if ($file !== "." && $file !== ".." &&  !pathinfo($file, PATHINFO_EXTENSION)) {
+                $path2 = "root/$file";
+                echo "<div><a href='index.php?files=$path2'><i class='bi bi-folder-fill'></i>" . $file . "</a></div>";
+                if (is_dir($path2) && $dh2 = opendir($path2)) {
+                    while (($file2 = readdir($dh2)) !== false) {
+                        if ($file2 !== "." && $file2 !== ".." &&  !pathinfo($file2, PATHINFO_EXTENSION)) {
+                            $path3 = "root/$file/$file2";
+                            echo "<div  class='subdirectories'><a href='index.php?files=$path3' class='anchor'><i class='bi bi-folder-fill'></i>" . $file2 . "</a></div>";
                         }
                     }
                 }
             }
-            closedir($dh);
         }
+        closedir($dh);
     }
 };
 
 
-function readDirect(){
-
-     if(isset($_GET["files"])){
-           
-        $dir =  $_GET["files"];
-     } 
-     
-     if(!isset($_GET["files"])){
-         Echo "Please select a folder";
-     }
+function readDirect()
+{
+    if (isset($_GET["files"])) {
+        $path =  $_GET["files"];
+        if (is_dir($path) && $dh = opendir($path)) {
+            while (($file = readdir($dh)) !== false) {
+                if ($file !== "." && $file !== ".." &&  pathinfo($file, PATHINFO_EXTENSION)) {
+                    echo "<div class = 'middleDivs'><a href='index.php?files=$path'>" . $file . "</a></div>";
+                } else if ($file !== "." && $file !== ".." &&  !pathinfo($file, PATHINFO_EXTENSION)) {
+                    echo "<div class = 'middleDivs'><a href='index.php?files=$path/$file'><i class='bi bi-folder-fill'></i>" . $file . "</a></div>";
+                }
+            }
+        }
+    }
+    if (!isset($_GET["files"])) {
+        echo "Please select a folder";
+    }
 };
